d82b462c27aa9966976661f31d7715c9cc6ac6a8
Henrikki Helander <henrikki_helander@hotmail.com>
2021-01-15 23:25:37 +0200

    Added addExtraData method

--- client/src/app/ship-radar/ship-radar.component.ts
+++ client/src/app/ship-radar/ship-radar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Ship } from '../models/ship.model';
 import { ShipService } from '../services/ship.service';
+import { getDistance } from 'geolib';
 
 @Component({
   selector: 'app-ship-radar',
@@ -9,25 +10,36 @@ import { ShipService } from '../services/ship.service';
 })
 export class ShipRadarComponent implements OnInit {
 ships: Ship[];
+homeCoordinates: [latitude: number, longitude: number] = [28.320951, 61.058983];
 
   constructor(private shipService: ShipService) {
    }
 
   ngOnInit() {
-
     this.shipService.getLatest().subscribe((res: any) => {
       this.ships = res.features;
-      this.calculateDistanceFromHome();
+      this.enterExtraData();
     });
   }
 
-
-  calculateDistanceFromHome(){
-    console.log("SHIPs: ", this.ships);
+  private enterExtraData() {
     for (let ship of this.ships)
     {
-      console.log(ship.geometry.coordinates[0])
+      this.addDistanceFromMustola(ship);
+      this.isHeadingTowardsMustola(ship);
     }
+  }
+
+  private addDistanceFromMustola(ship: Ship) {
+    ship.properties.distance = getDistance(
+      {latitude: this.homeCoordinates[0], longitude: this.homeCoordinates[1]},
+      {latitude: ship.geometry.coordinates[1], longitude: ship.geometry.coordinates[0]});
+  }
+
+  private isHeadingTowardsMustola(ship: Ship) {
+    console.log(ship);
+    // (lat < 61.0804652 and long > 28.2754649)) and ((((200 <= course <= 360) or course <= 10) and lat < homeLat and long > homeLong) or ((30 <= course <= 190) and lat > homeLat and long < homeLong)):
+
   }
 
 }
