66c43593c158282392027427e30759efc03fbf0c
Henrikki Helander <henrikki_helander@hotmail.com>
2021-01-28 23:12:46 +0200

    Added routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InfoComponent } from './info/info.component';
+import { ShipRadarComponent } from './ship-radar/ship-radar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/ships', pathMatch: 'full' },
+  { path: 'info', component: InfoComponent },
+  { path: 'ships', component: ShipRadarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
